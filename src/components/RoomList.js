f84c5fea539571a5692ef8c0796a4f74665f7683
Joseph <joseph@Josephs-MacBook-Pro.local>
2019-03-09 00:11:55 -0500

    checkpoint 3 done- create cchat rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,10 +5,17 @@ class RoomList extends Component {
     super(props);
       this.state = {
           rooms: [],
-          newRoomName: '',
-          name: ''
+          newRoomName: "",
       };
     this.roomsRef = this.props.firebase.database().ref('rooms');
+
+    }
+   createNewRoom(e) {
+        e.preventDefault();
+        if (this.handleSubmit(this.state.newRoomName)) {
+          this.roomsRef.push({ name: this.state.newRoomName });
+          this.setState({ newRoomName: "" });
+        }
     }
 componentDidMount() {
     this.roomsRef.on('child_added', snapshot => {
@@ -18,21 +25,40 @@ componentDidMount() {
         room.key = snapshot.key;
         this.setState({ rooms: this.state.rooms.concat( room ) })
     });
-     
 }
-    
+  
+handleChange(e) {
+    e.preventDefault();
+    this.setState({ newRoomName: e.target.value });
+}
+    handleSubmit(newRoomName) {
+        const newRoomLength = newRoomName.trim().length;
+        if (newRoomLength > 0) {
+          return true;
+        } else {
+          return false;
+}
+    } 
    render() {
     return (
        <section className="room-list">
        <h3>Chat Rooms</h3>
-           {this.state.rooms.map( room => 
-               <li key={room.key} >
-                 {room.name}
-               </li>
-           )}
-       </section>
+       <ul className='chat'>
+       {this.state.rooms.map((room) => (
+            <li key={room} className="roomname">
+              {room.name}
+              </li>
+       ))}
+           </ul>
+           <form id="addRoomForm" onSubmit={e => this.createNewRoom(e)}>
+          <fieldset>
+            <legend>Create New Chat Room</legend>
+            <input type="text" name="newRoomName" placeholder="New Room Name" onChange={e => this.handleChange(e)} />
+            <input type="submit" value="Create" />
+          </fieldset>
+        </form>
+        </section>
     );
   }
 }
-
 export default RoomList;
